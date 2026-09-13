1087f5914f6b8c38530bba4e4a028f08c189d000
nsmartonline8-cloud <nsmartonline8@gmail.com>
2026-09-13 15:32:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -711,7 +711,7 @@ st.markdown(
 
     /* DELETE title: smaller text only */
     .payment-delete-title {
-        font-size: 10px !important;
+        font-size: 8px !important;
         font-weight: 600 !important;
         line-height: 1.1 !important;
         margin: 0 0 2px 0 !important;
