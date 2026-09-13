0878fac096f1efcbc78f49adc37398a723a112c4
nsmartonline8-cloud <nsmartonline8@gmail.com>
2026-09-13 11:04:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3746,7 +3746,7 @@ deed_heading, first_title, second_title = document_names(selected_document)
 # TOP DOCUMENT CONTROLS
 # =========================================================
 
-date_column, search_column, txn_column = st.columns([0.42, 0.483, 0.351])
+date_column, search_column, txn_column = st.columns([0.21, 0.2415, 0.1755])
 
 with date_column:
     st.date_input(
